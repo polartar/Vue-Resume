b7bd782fa21bed2958ee1d1681cd962262139e72
Geoffrey Rose <geoffrey@geoffreyrosedesign.com>
2020-10-05 16:27:44 -0700

    remove un need fb tracking

--- resources/views/partials/tracking_noscripts.blade.php
+++ resources/views/partials/tracking_noscripts.blade.php
@@ -1,9 +1,3 @@
 {{-- Google Tag Manager (noscript) --}}
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.google.tag_manager_id') }}{!! config('services.google.tag_manager_env') !!}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 {{-- End Google Tag Manager (noscript) --}}
-
-@if(app()->environment('production'))
-{{-- Facebook Pixel (noscript) --}}
-<noscript><img height="1" width="1" src="https://www.facebook.com/tr?id={{ config('services.facebook.tracking_pixel_id') }}&ev=PageView&noscript=1{{ $fb_purchase }}"/></noscript>
-{{-- End Facebook Pixel (noscript) --}}
-@endif
