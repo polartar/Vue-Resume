fead4dd74f45abc860cf31ce9672c5d5fd2c162f
Dennis Sauve <dengsauve@yahoo.com>
2020-10-07 12:08:27 -0700

    Updated resume summary

--- resources/assets/js/views/steps/ResumeSummary.vue
+++ resources/assets/js/views/steps/ResumeSummary.vue
@@ -1,20 +1,38 @@
 <template>
     <div>
-        <h3>Resume Summary</h3>
+        <h3>
+            Resume Summary
+            <small style="text-decoration: underline; cursor: pointer;" @click="show = !show">
+                <span v-if="!show">Create</span><span v-else>See All Summaries</span>
+            </small>
+        </h3>
 
-        <div class="resume-step-form">
-            <div class="grid-x grid-margin-x">
-                <div class="cell">
-                    <div class="form-group">
+        <div class='resume-step-form' v-if="show">
+            <div class='grid-x grid-margin-x'>
+                <div class='cell'>
+                    <div class='form-group'>
                         <label>Summary</label>
-                        <textarea v-model="name" required name="summary" rows="8" cols="80" />
+                        <textarea rows='8' cols='80' v-model='name' />
                     </div>
                 </div>
-                <div class="cell medium">
-                    <div class="form-group">
-                        <button class="button" @click="$router.go(-1)" style="background-color: grey!important;">Back</button>
-                        <router-link tag="button" class="button" v-on:click.native="saveSummary" to="work-experience">Save and Next</router-link>
+            </div>
+            <button class='button' type='button' @click='saveSummary'>Add Summary</button>
+        </div>
+        <div class='resume-step-form' v-if="!show">
+            <div class='grid-x grid-margin-x'>
+                <div class="cell" v-for="summary in resume.resume_summaries" :key="summary.id">
+                    <div class="card">
+                        <div class="card-section">
+                            <p class="float-left" style="margin-bottom: 0;">
+                                {{ summary.name }}
+                            </p>
+                            <button class="button close-button float-right" @click="removeSummary(summary.id)">X</button>
+                        </div>
                     </div>
+                </div>
+                <div class="cell">
+                    <button class="button" @click="$router.go(-1)" style="background-color: grey!important;">Back</button>
+                    <router-link tag="button" class="button" to="work-experience">Next</router-link>
                 </div>
             </div>
         </div>
@@ -32,15 +50,39 @@
         data: function () {
             return {
                 'name': '',
+                'show': false,
             }
         },
         methods: {
             saveSummary: function () {
+                this.show = false;
                 this.$axios
                     .post('/resume-summary', {
                         'resume_id': this.$store.state.resume.id,
                         'name': this.name,
                     })
+                    .then(response => {
+                        this.$toasted.show('Successfully updated the resume summary', {
+                            position: 'bottom-center',
+                            duration: 3000,
+                            fullWidth: true,
+                            type: 'success',
+                        });
+                        this.$store.commit('reloadResume');
+                        this.name = '';
+                    })
+                    .catch(error => {
+                        this.$toasted.show('Uh oh, we had some trouble with that.', {
+                            position: 'bottom-center',
+                            duration: 3000,
+                            fullWidth: true,
+                            type: 'error',
+                        });
+                    });
+            },
+            removeSummary: function (summaryId) {
+                this.$axios
+                    .delete('/resume-summary/' + summaryId)
                     .then(response => {
                         this.$toasted.show('Successfully updated the resume summary', {
                             position: 'bottom-center',
@@ -58,7 +100,7 @@
                             type: 'error',
                         });
                     });
-            },
+            }
         }
     }
 </script>
