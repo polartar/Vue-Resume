0c4bc9f3a578eac62dbf6d4d2a745a847a9b9d5f
talentdev2020 <polaris.center@outlook.com>
2021-02-24 17:54:37 -0700

    Modify the skill name on Golden standard

--- resources/assets/js/views/resume-templates/GoldenStandard.vue
+++ resources/assets/js/views/resume-templates/GoldenStandard.vue
@@ -118,7 +118,7 @@
         </div>
 
         <div class="full-width-table resume-section" ref='skill'   >
-               <div class="section-heading" >Techniques, Software, & Instrumentation</div>
+               <div class="section-heading" >Skills</div>
                <div class="skill-body">
                     <div class="skill-row">
                         <div class='skill-column'  v-for="skill in resume.resume_skill" v-bind:key="skill.id">
