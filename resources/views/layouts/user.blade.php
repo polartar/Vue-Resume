413f3f4bb55d579177727fd2be5f4908f49da609
Employee <employee@Denniss-Work-iMac.local>
2021-03-08 16:24:14 -0800

    added helpcrunch widget

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -64,5 +64,22 @@
 
     @stack('scripts')
     @include('partials.tracking_scripts_footer')
+
+    <script type="text/javascript">
+    (function(w,d){
+    w.HelpCrunch=function(){w.HelpCrunch.q.push(arguments)};w.HelpCrunch.q=[];
+    function r(){var s=document.createElement('script');s.async=1;s.type='text/javascript';s.src='https://widget.helpcrunch.com/';(d.body||d.head).appendChild(s);}
+    if(w.attachEvent){w.attachEvent('onload',r)}else{w.addEventListener('load',r,false)}
+    })(window, document)
+    </script>
+
+    <script type="text/javascript">
+    HelpCrunch('init', 'cheekyscientist', {
+        applicationId: 1,
+        applicationSecret: 'PlSBnd33l3Oc0ymn9huyp9DXimpzbU0key9JaCpbROsO/j+FfgBfzXiSaRIJeqRXh6jVTH52HuNpK8pDU5I0lw=='
+    })
+
+    HelpCrunch('showChatWidget');
+    </script>
 </body>
 </html>
