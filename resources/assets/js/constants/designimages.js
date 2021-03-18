87a7fc0f0291f3894f7c59a29db0ec8dc6750bb2
talentdev2020 <polaris.center@outlook.com>
2021-03-17 17:10:51 -0700

    Update design images

--- resources/assets/js/constants/designimages.js
+++ resources/assets/js/constants/designimages.js
@@ -1,7 +1,7 @@
 const DesignImages = {
     "Gold Standard": "/assets/images/designs/gold.png",
     "Functional": "/assets/images/designs/functional.png",
-    "Recruiter": "/assets/images/designs/recuriter.png",
+    "Recruiter": "/assets/images/designs/recruiter.png",
     "Combination": "/assets/images/designs/combination.png",
     "Sidebar": "/assets/images/designs/sidebar.png",
 }
